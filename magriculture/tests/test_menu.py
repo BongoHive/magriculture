f5ff72cd0269049b55d4b55f36dd6e018d7d385d
dmaclay <dmaclay@gmail.com>
2011-05-09 15:00:41 +0200

    just richard

---
+++ magriculture/tests/test_menu.py
@@ -0,0 +1,133 @@
+import time
+from twisted.trial.unittest import TestCase
+from magriculture.workers.menus import MenuConsumer, MenuPublisher, MenuWorker
+
+class MenuTestCase(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_menu(self):
+
+        test_yaml = '''
+        __data__:
+            url:
+            username:
+            password:
+            json: >
+                {
+                    "farmers": [
+                        {
+                            "name": "Richard",
+                            "cows": [
+                                {
+                                    "name": "dairy",
+                                    "quantityMilked": 0,
+                                    "milkTimestamp": 0,
+                                    "cowRegId": "reg1",
+                                    "quantitySold": 0
+                                },
+                                {
+                                    "name": "dell",
+                                    "quantityMilked": 0,
+                                    "milkTimestamp": 0,
+                                    "cowRegId": "reg2",
+                                    "quantitySold": 0
+                                }
+                            ],
+                            "timestamp": "1294843761",
+                            "farmerRegId": "frm1"
+                        }
+                    ],
+                    "msisdn": "456789"
+                }
+
+        __start__:
+            display:
+                english: "Hello."
+            next: farmers
+
+        farmers:
+            question:
+                english: "Hi. There are multiple farmers with this phone number. Who are you?"
+            options: name
+            next: cows
+
+        cows:
+            question:
+                english: "For which cow would you like to submit a milk collection?"
+            options: name
+            next: quantityMilked
+
+        quantityMilked:
+            question:
+                english: "How much milk was collected?"
+            validate: integer
+            next: quantitySold
+
+        quantitySold:
+            question:
+                english: "How much milk did you sell?"
+            validate: integer
+            next: milkTimestamp
+
+        milkTimestamp:
+            question:
+                english: "When was this collection done?"
+            options:
+                  - display:
+                        english: "Today"
+                    default: today
+                    next: __finish__
+                  - display:
+                        english: "Yesterday"
+                    default: yesterday
+                    next: __finish__
+                  - display:
+                        english: "An earlier day"
+                    next:
+                        question:
+                            english: "Which day was it [dd/mm/yyyy]?"
+                        validate: date
+                        next: __finish__
+
+        __finish__:
+            display:
+                english: "Thank you! Your milk collection
+                was registered successfully."
+        '''
+
+
+        sess_cons = MenuConsumer(None)
+        sess_cons.set_yaml_template(test_yaml)
+        sess4 = sess_cons.get_session("12345")
+        dt4 = sess4.get_decision_tree()
+
+        dt4.echo_on()
+        repr(dt4.start())
+        #repr(dt4.question())
+        #dt4.answer(1)
+        repr(dt4.question())
+        dt4.answer(1)
+        repr(dt4.question())
+        dt4.answer(42)
+        repr(dt4.question())
+        dt4.answer(23)
+        repr(dt4.question())
+        dt4.answer(3)
+        repr(dt4.question())
+        dt4.answer("03/03/2011")
+        repr(dt4.finish())
+
+        print ''
+        print repr(dt4.get_data_source())
+        print sess4.get_decision_tree().dump_json_data()
+
+
+
+
+        #print "\n\n"
+        #time.sleep(2)
--- magriculture/workers/menus.py
+++ magriculture/workers/menus.py
@@ -1,56 +1,13 @@
-from twisted.python import log
-from twisted.python.log import logging
-from twisted.internet.defer import inlineCallbacks, returnValue
+from vumi.workers.session.worker import SessionConsumer, SessionPublisher, SessionWorker
 
-from vumi.service import Worker, Consumer, Publisher
-from vumi.session import VumiSession
-from vumi.message import Message, VUMI_DATE_FORMAT
-
-from twisted.python import log
-from twisted.python.log import logging
-from twisted.internet.defer import inlineCallbacks, returnValue
-
-class MenuConsumer(Consumer):
-    exchange_name = "vumi"
-    exchange_type = "direct"
-    durable = True
-    delivery_mode = 2
+class MenuConsumer(SessionConsumer):
     queue_name = "magri.inbound.menu_trans.base" #TODO revise name
     routing_key = "magri.inbound.menu_trans.base" #TODO revise name
-    sessions = {}
 
-    def __init__(self, publisher):
-        self.publisher = publisher
-
-    def consume_message(self, message):
-        log.msg("menu message %s consumed by %s" % (json.dumps(dictionary),self.__class__.__name__))
-        #dictionary = message.get('short_message')
-
-    def call_for_json(self, MSISDN, farmer_name):
-        if 1 == 2:
-            pass
-        else:
-            log.msg("OMG the interwebs are broken!")
-            return None
-
-
-    def get_session(self, MSISDN):
-        session = self.sessions.get(MSISDN)
-        if not session:
-            session = VumiSession()
-            self.sessions[MSISDN] = session
-        return session
-
-
-class MenuPublisher(Publisher):
-    exchange_name = "vumi"
-    exchange_type = "direct"
+class MenuPublisher(SessionPublisher):
     routing_key = "magri.outbound.menu_trans.fallback"
-    durable = True
-    auto_delete = False
-    delivery_mode = 2
 
-    def publish_message(self, message, **kwargs):
-        log.msg("Publishing Message %s with extra args: %s" % (message, kwargs))
-        super(MenuPublisher, self).publish_message(message, **kwargs)
+class MenuWorker(SessionWorker):
+    pass
+
 
