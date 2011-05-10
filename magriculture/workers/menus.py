eeedff1d217730a58110fcc27c0573f87802b8be
dmaclay <dmaclay@gmail.com>
2011-05-10 08:56:03 +0200

    argh

--- magriculture/workers/menus.py
+++ magriculture/workers/menus.py
@@ -1,13 +1,23 @@
+from twisted.python import log
+from twisted.python.log import logging
+from twisted.internet.defer import inlineCallbacks, returnValue
+
 from vumi.workers.session.worker import SessionConsumer, SessionPublisher, SessionWorker
 
 class MenuConsumer(SessionConsumer):
-    queue_name = "magri.inbound.menu_trans.base" #TODO revise name
-    routing_key = "magri.inbound.menu_trans.base" #TODO revise name
+    queue_name = "magri.inbound.lactation.default"
+    routing_key = "magri.inbound.lactation.default"
 
 class MenuPublisher(SessionPublisher):
-    routing_key = "magri.outbound.menu_trans.fallback"
+    routing_key = "magri.outbound.lactation.default"
 
 class MenuWorker(SessionWorker):
-    pass
+    @inlineCallbacks
+    def startWorker(self):
+        log.msg("Starting the MenuWorker")
+        self.publisher = yield self.start_publisher(MenuPublisher)
+        yield self.start_consumer(MenuConsumer, self.publisher)
 
+    def stopWorker(self):
+        log.msg("Stopping the MenuWorker")
 
