d13e3c53e45097b166d01dfdb5529653dcbf4122
Simon de Haan <simon@praekeltfoundation.org>
2011-07-28 02:15:45 +0200

    cleaner imports

--- magriculture/fncs/models/__init__.py
+++ magriculture/fncs/models/__init__.py
@@ -1,3 +1,6 @@
-from magriculture.fncs.models.geo import *
-from magriculture.fncs.models.actors import *
-from magriculture.fncs.models.props import *
+from magriculture.fncs.models.geo import (Province, RPIArea, Zone, District,
+    Ward, Village, Market)
+from magriculture.fncs.models.actors import (Actor, Farmer, FarmerGroup, 
+    ExtensionOfficer, MarketMonitor, Agent)
+from magriculture.fncs.models.props import (Crop, CropUnit, Transaction, 
+    Offer)
