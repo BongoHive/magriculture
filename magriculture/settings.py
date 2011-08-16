d85fc1d62c5008d27413cd040ff1fcd0106bfbf4
Simon de Haan <simon@praekeltfoundation.org>
2011-08-16 10:43:13 +0200

    getting jenkins to work

--- magriculture/settings.py
+++ magriculture/settings.py
@@ -21,7 +21,7 @@ DATABASES = {
         'NAME': 'magriculture',                      # Or path to database file if using sqlite3.
         'USER': 'magriculture',                      # Not used with sqlite3.
         'PASSWORD': 'magriculture',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
