4489d2e179c4bc75c322bec74667dfeefc52c561
Simon de Haan <simon@praekeltfoundation.org>
2011-08-23 23:52:20 +0200

    auth perms

--- magriculture/utils/resetdb.sh
+++ magriculture/utils/resetdb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-dropdb magriculture
-createdb magriculture
+dropdb --username magriculture -W --host localhost magriculture
+createdb --username magriculture -W --host localhost magriculture
 ./manage.py syncdb --migrate --noinput
 echo "Creating superuser"
 ./manage.py createsuperuser
