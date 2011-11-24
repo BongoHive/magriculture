26ce0e9880316a57348f9fa94b98319ff2542607
Simon Cross <hodgestar@gmail.com>
2011-11-24 18:17:01 +0200

    Fix agent creation when farmer is explicitly specified.

--- magriculture/fncs/management/commands/fncs_create_sample_farmers.py
+++ magriculture/fncs/management/commands/fncs_create_sample_farmers.py
@@ -3,6 +3,7 @@ from magriculture.fncs.tests import utils
 from magriculture.fncs.models.actors import Actor
 from optparse import make_option
 
+
 class Command(BaseCommand):
     help = "Generate sample farmer data"
     option_list = BaseCommand.option_list + (
@@ -10,7 +11,7 @@ class Command(BaseCommand):
                         help='How many farmers to create'),
         make_option('--agent', dest='agent', type='str', default=None,
                         help='Generate farmers for a specific agent'),
-        make_option('--farmer', dest='farmer', type='str', default=None,
+        make_option('--farmer', dest='farmer', type='int', default=None,
                         help='Generate sample data for a specific farmer'
                              ' (misisdn)'),
     )
@@ -20,37 +21,37 @@ class Command(BaseCommand):
         username = options['agent']
         farmer = options['farmer']
         if farmer:
-            msisdns = [farmer]
+            msisdns = [int(farmer)]
         else:
-            msisdns = [str(2776123456 + i) for i in range(total)]
+            msisdns = [2776123456 + i for i in range(total)]
         if username:
             actor = Actor.objects.get(user__username=username)
             agent = actor.as_agent()
             self.generate_farmers(msisdns, agent)
         else:
+            # create the agent with msisdn offset of the total generated to
+            # avoid collissions on usernames
+            agent_msisdn = 2776123456 + total + 1
+            agent = utils.create_agent(msisdn=str(agent_msisdn),
+                        name=utils.random_name(),
+                        surname=utils.random_surname())
             self.generate_farmers(msisdns)
 
-    def generate_farmers(self, msisdns, agent=None):
+    def generate_farmers(self, msisdns, agent):
         for msisdn in msisdns:
             # create a district
             district = utils.random_district()
             farmergroup_name = "%s Farmer Group" % (district.name,)
 
             # create the farmer
-            farmer = utils.create_farmer(msisdn=str(msisdn), name=utils.random_name(),
-                        surname=utils.random_surname(), farmergroup_name=farmergroup_name)
+            farmer = utils.create_farmer(msisdn=str(msisdn),
+                                         name=utils.random_name(),
+                                         surname=utils.random_surname(),
+                                         farmergroup_name=farmergroup_name)
 
             # cultivates two types of crops
             farmer.grows_crop(utils.random_crop())
             farmer.grows_crop(utils.random_crop())
-
-            # create the agent with msisdn offset of the total generated to
-            # avoid collissions on usernames
-            msisdn = msisdn + total + 1
-            if not agent:
-                agent = utils.create_agent(msisdn=str(msisdn),
-                            name=utils.random_name(),
-                            surname=utils.random_surname())
 
             # create a market in the district
             market_name = '%s Market' % district.name
@@ -58,4 +59,3 @@ class Command(BaseCommand):
 
             # have the farmer sells crops at that market through the agent
             farmer.operates_at(market, agent)
-
