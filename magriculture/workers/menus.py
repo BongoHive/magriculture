6bb36a3342b8bd1670b03d362302ef15280ce8cc
dmaclay <dmaclay@gmail.com>
2012-02-10 01:40:30 +0200

    SessionApplicationWorker to add session to ApplicatioWorker

--- magriculture/workers/menus.py
+++ magriculture/workers/menus.py
@@ -297,7 +297,47 @@ class MMMobileMenuWorker(SessionWorker):
         log.msg("Stopping the MenuWorker")
 
 
-class LactationWorker(ApplicationWorker):
+class SessionApplicationWorker(ApplicationWorker):
+
+    def set_yaml_template(self, yaml_template):
+        self.yaml_template = yaml_template
+
+    def set_data_url(self, data_source):
+        self.data_url = data_source
+
+    def set_post_url(self, post_source):
+        self.post_url = post_source
+
+    def consume_message(self, message):
+        # TODO: Eep! This code is broken!
+        log.msg("session message %s consumed by %s" % (
+            json.dumps(dictionary), self.__class__.__name__))
+        #dictionary = message.get('short_message')
+
+    def get_session(self, MSISDN):
+        sess = getVumiSession(self.r_server,
+                              self.routing_key + '.' + MSISDN)
+        if not sess.get_decision_tree():
+            sess.set_decision_tree(self.setup_new_decision_tree(MSISDN))
+        return sess
+
+    def del_session(self, MSISDN):
+        return delVumiSession(self.r_server, MSISDN)
+
+    def setup_new_decision_tree(self, MSISDN, **kwargs):
+        decision_tree = TraversedDecisionTree()
+        yaml_template = self.yaml_template
+        decision_tree.load_yaml_template(yaml_template)
+        self.set_data_url(decision_tree.get_data_source())
+        self.set_post_url(decision_tree.get_post_source())
+        if self.data_url.get('url'):
+            raise ValueError("This is broken. Sorry. :-(")
+        else:
+            decision_tree.load_dummy_data()
+        return decision_tree
+
+
+class LactationWorker(SessionApplicationWorker):
     """A worker that presents a USSD menu allowing farmers to
     record cows milk production.
 
@@ -326,6 +366,5 @@ class LactationWorker(ApplicationWorker):
     @inlineCallbacks
     def consume_user_message(self, msg):
         user_id = msg.user()
-        #print user_id
         self.reply_to(msg, "TEST", False)
         yield None
