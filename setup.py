183ba94c4dda25f0ccfb8687c80a30bd700d26f4
Simon Cross <hodgestar@gmail.com>
2012-11-15 09:48:55 +0200

    PEP8 clean-up of setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
 from setuptools import setup, find_packages
 
+
 def listify(filename):
-    return filter(None, open(filename,'r').read().split('\n'))
+    return filter(None, open(filename, 'r').read().split('\n'))
+
 
 def remove_externals(requirements):
     return filter(lambda e: not e.startswith('-e'), requirements)
 
 setup(
-    name = "magriculture",
-    version = "0.1.0",
-    url = 'http://github.com/praekelt/magriculture',
-    license = '?',
-    description = "Tool to provide market information to "
-                  "smallholding farmers in emerging markets.",
-    long_description = open('README.rst','r').read(),
-    author = 'Praekelt Foundation',
-    author_email = 'dev@praekeltfoundation.org',
-    packages = find_packages(),
-    install_requires = ['setuptools'] +
+    name="magriculture",
+    version="0.1.0",
+    url='http://github.com/praekelt/magriculture',
+    license='?',
+    description="Tool to provide market information to "
+                "smallholding farmers in emerging markets.",
+    long_description=open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekeltfoundation.org',
+    packages=find_packages(),
+    install_requires=['setuptools'] +
         remove_externals(listify('requirements.pip')),
-    classifiers = [
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Private Developers',
         'License :: ? :: ?',
@@ -29,4 +31,3 @@ setup(
         'Topic :: System :: Networking'
     ]
 )
-
