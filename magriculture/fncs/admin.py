672ce10ff01cb4bc94ac3f58a654f262afa37bf6
Simon Cross <hodgestar@gmail.com>
2013-05-06 16:04:05 +0200

    Specify custom search and display fields for actors, agents and farmers.

--- magriculture/fncs/admin.py
+++ magriculture/fncs/admin.py
@@ -1,12 +1,28 @@
 from django.contrib import admin
 from magriculture.fncs.models import actors, props, geo
 
-admin.site.register(actors.Actor)
-admin.site.register(actors.Farmer)
+
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'gender')
+    search_fields = ('name',)
+
+
+class FarmerAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'actor')
+    search_fields = ('actor__name',)
+
+
+class AgentAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'actor')
+    search_fields = ('actor__name',)
+
+
+admin.site.register(actors.Actor, ActorAdmin)
+admin.site.register(actors.Farmer, FarmerAdmin)
+admin.site.register(actors.Agent, AgentAdmin)
 admin.site.register(actors.FarmerGroup)
 admin.site.register(actors.ExtensionOfficer)
 admin.site.register(actors.MarketMonitor)
-admin.site.register(actors.Agent)
 
 admin.site.register(props.Crop)
 admin.site.register(props.CropUnit)
