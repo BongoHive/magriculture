576e0c314345cf0d6f1afd8d5ac5b59a03146adc
Mike Jones <mike@westerncapelabs.com>
2013-11-20 15:35:50 +0200

    Add tests for app api

--- go-js/test/test_magri_ussd.js
+++ go-js/test/test_magri_ussd.js
@@ -6,6 +6,21 @@ var app = require("../lib/magri-ussd");
 var test_utils = vumigo.test_utils;
 
 
+// This just checks that you hooked you InteractionMachine
+// up to the api correctly and called im.attach();
+describe("test_api", function() {
+    it("should exist", function() {
+        assert.ok(app.api);
+    });
+    it("should have an on_inbound_message method", function() {
+        assert.ok(app.api.on_inbound_message);
+    });
+    it("should have an on_inbound_event method", function() {
+        assert.ok(app.api.on_inbound_event);
+    });
+});
+
+
 describe("test menu worker", function() {
     var tester = new test_utils.ImTester(app.api, {
         max_response_length: 160
