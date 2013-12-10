62966c623d794fb99c3ef8508a310a83d91e0181
gitumarkk <gitumarkk@gmail.com>
2013-12-10 14:14:10 +0200

    added decorator for extension_officer

---
+++ magriculture/fncs/decorators.py
@@ -0,0 +1,19 @@
+# Python
+from functools import wraps
+
+# Django
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
+from django.contrib import messages
+
+
+def extension_officer_required(function):
+    @wraps(function)
+    def decorated_function(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            if not request.user.actor.as_extensionofficer():
+                messages.error(request, "You need to be an extension officer to view that.")
+                return redirect(reverse("login"))
+
+        return function(request, *args, **kwargs)
+    return decorated_function
