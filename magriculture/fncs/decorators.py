1b96fde9f09dcd7014121ba2d3f3ea4b5679699e
gitumarkk <gitumarkk@gmail.com>
2013-12-11 17:29:47 +0200

    created the agent_required decorator

--- magriculture/fncs/decorators.py
+++ magriculture/fncs/decorators.py
@@ -17,3 +17,15 @@ def extension_officer_required(function):
 
         return function(request, *args, **kwargs)
     return decorated_function
+
+
+def agent_required(function):
+    @wraps(function)
+    def decorated_function(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            if not request.user.actor.as_agent():
+                messages.error(request, "You need to be an extension officer to view that.")
+                return redirect(reverse("login"))
+
+        return function(request, *args, **kwargs)
+    return decorated_function
