2f1fb89aa448ed00e25ecddfc7b0eb2b2b857e8b
gitumarkk <gitumarkk@gmail.com>
2014-01-20 17:12:25 +0200

    updated tasks to pass tests

--- magriculture/fncs/tasks.py
+++ magriculture/fncs/tasks.py
@@ -6,8 +6,10 @@ from magriculture.fncs.models.actors import Farmer, FarmerGroup, Agent
 from magriculture.fncs.models.props import CropReceipt
 
 # Celery
+from celery.utils.log import get_task_logger
 from celery.decorators import task
 
+logger = get_task_logger(__name__)
 
 @task
 def query_crop_receipt_for_old_crops():
@@ -15,8 +17,9 @@ def query_crop_receipt_for_old_crops():
     This will form the celery worker that will be run to query
     the crop reciept for old stock
     """
+    logger.info("Performing query for old Receipts")
     days_3_ago = datetime.today() - timedelta(days=3)
-    crop_receipts = (CropReciept.objects.
+    crop_receipts = (CropReceipt.objects.
                     filter(reconciled=False).
                     filter(created_at__lt=days_3_ago).
                     all())
@@ -25,14 +28,19 @@ def query_crop_receipt_for_old_crops():
         check_inventory_left(crop_receipt)
 
 
-
 def check_inventory_left(crop_receipt):
     """
     Function that checks inventory to see if there
-    is any inventory left
+    is any inventory left.
     """
+    logger.info("Running the inventory chekcer and send message")
+
+    # Assuming that something went wrong and Reciepts with 0 crops is not reconciled
     if crop_receipt.remaining_inventory() > 0:
         recipient = crop_receipt.farmer.actor
-        message = "These crops were unable to be sold"
-        crop_receipt.agent.actor.send_message(recipient, message)
+        crop = crop_receipt.crop.name
+        remaining = crop_receipt.remaining_inventory()
+        message = "Sorry but %s of %s have not been sold" % (remaining, crop)
+        crop_receipt.agent.actor.send_message(recipient, message, None)
         crop_receipt.reconciled = True
+        crop_receipt.save()
