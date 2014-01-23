303e945a022d59620d4d14bdda05e6d9ae29eab4
gitumarkk <gitumarkk@gmail.com>
2014-01-23 14:33:24 +0200

    created the call command function

--- magriculture/fncs/management/commands/fncs_add_market_districts_for_farmers_with_no_districts.py
+++ magriculture/fncs/management/commands/fncs_add_market_districts_for_farmers_with_no_districts.py
@@ -1,10 +1,19 @@
-from magriculture.fncs.utils import ImportCommand
+# Django
+from django.core.management.base import BaseCommand
+
+# Project
+from magriculture.fncs.models.actors import Farmer
 
 
-class Command(ImportCommand):
+class Command(BaseCommand):
     help = ("If a farmer doesn't have a district, the farmers markets"
             " districts are assumed to be his market")
 
 
     def handle(self, *args, **options):
-        pass
+        # Getting farmers with no districts
+        farmers_no_distr = Farmer.objects.filter(districts=None).all()
+        for farmer in farmers_no_distr:
+            # Getting the markets for the farmer
+            for market in farmer.markets.all():
+                farmer.districts.add(market.district)
