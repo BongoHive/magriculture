53ada7e202e8c3ccce42f3b990e29030d369414c
gsvr <gsvanrooyen@gmail.com>
2014-07-17 09:04:25 +0200

    print - stdout, remove --total

--- magriculture/fncs/management/commands/fncs_users_find_bad_lengths.py
+++ magriculture/fncs/management/commands/fncs_users_find_bad_lengths.py
@@ -1,41 +1,33 @@
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
-from optparse import make_option
 
 # Example usage:
-# python manage.py fncs_users_find_bad_lengths --total 30
+# python manage.py fncs_users_find_bad_lengths
 
 
 class Command(BaseCommand):
-    help = "Search for too long and too short numbers"
-    option_list = BaseCommand.option_list + (
-        make_option('--total', dest='total', type='int', default=15000,
-                        help='How many users to delete'),
-    )
+    help = "Search for too long and too short numbers (+260 numbers only)"
 
     def handle(self, *args, **options):
-        total = options['total']
-
-
-        users = User.objects.filter(username__startswith='+260')[0:total]
+        users = User.objects.filter(username__startswith='+260')
         total = len(users)
         expected_length = 13
 
         if total == 0:
             self.stdout.write('No users starting with +260\n')
 
-        print ('Short usernames:')
+        self.stdout.write('Short usernames:\n')
 
         for user in users:
             stripped = user.username.replace(' ', '')
             length = len(stripped)
             if length < expected_length:
-                print user.username
+                self.stdout.write(user.username + '\n')
 
-        print ('Long usernames:')
+        self.stdout.write('\nLong usernames:\n')
 
         for user in users:
             stripped = user.username.replace(' ', '')
             length = len(stripped)
             if length > expected_length:
-                print user.username
+                self.stdout.write(user.username + '\n')
